ed47c2aa5a6a969c103d4d18212210e146ba8aaa
RainingQ <275922122@qq.com>
2017-11-12 16:38:16 +0800

    clean

--- YourEyes_VehicleDetectionTracking/YourEyes_VehicleDetectionTracking/BackgroundSubstraction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __BACKGROUNDSUBSTRACTION_H_
-#define __BACKGROUNDSUBSTRACTION_H_
-#include <opencv2/opencv.hpp>
-using namespace cv;
-
-void backgroundSubstraction(const Mat& srcImage1, const Mat& srcImage2, Mat& diffImage, Mat& threshImage);
-
-#endif
