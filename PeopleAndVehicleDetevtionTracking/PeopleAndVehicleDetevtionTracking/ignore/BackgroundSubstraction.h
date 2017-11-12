89e1817d90c090018e3afd7d3f73fd99740b658e
RainingQ <275922122@qq.com>
2017-11-12 11:27:45 +0800

    sprint(10/10)

--- PeopleAndVehicleDetevtionTracking/PeopleAndVehicleDetevtionTracking/ignore/BackgroundSubstraction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __BACKGROUNDSUBSTRACTION_H_
-#define __BACKGROUNDSUBSTRACTION_H_
-#include <opencv2/opencv.hpp>
-using namespace cv;
-
-void backgroundSubstraction(const Mat& cy_srcImage1, const Mat& cy_srcImage2, Mat& threshImage);
-
-#endif
